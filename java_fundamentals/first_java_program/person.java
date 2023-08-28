d2e20c8c8cb537478b33573e13610215f94739a5
isaacsbm <isaacsonbm@gmail.com>
2023-08-28 10:50:40 -0700

    main

---
+++ java_fundamentals/first_java_program/person.java
@@ -0,0 +1,9 @@
+package first_java_program;
+
+public class person {
+    public static void main(String[] args) {
+        System.out.println("My name is Anakin");
+        System.out.println("I am 45 years old");
+        System.out.println("My hometown is Tatooine");
+    }
+}
